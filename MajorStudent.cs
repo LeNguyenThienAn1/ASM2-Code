3f475835bdcc583b8d8c283626d1f10b9de6975e
LeNguyenThienAn1 <110837183+LeNguyenThienAn1@users.noreply.github.com>
2023-06-22 18:48:22 +0700

    Update MajorStudent.cs

--- MajorStudent.cs
+++ MajorStudent.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +9,17 @@ namespace QLSV3
     public class MajorStudent : Student
     {
         public string Major { get; set; }
+        public int Year { get; set; }
 
         public override void PrintInfo()
         {
-            base.PrintInfo();
+            Console.WriteLine("Name Of Student: " + Name);
+            Console.WriteLine("Age Student: " + Age);
+            Console.WriteLine("ID Student: " + ID);
+            Console.WriteLine("Gender Student: " + Gender);
             Console.WriteLine("Major: " + Major);
+            Console.WriteLine("Year: " + Year);
         }
     }
 }
+
